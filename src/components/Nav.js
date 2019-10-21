d6490a4df223b49973dcab5b154f543c65c1b773
umekow <umekolashun@gmail.com>
2019-10-21 17:12:03 -0500

    started on nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import { NavLink } from 'react-router-dom'; 
+
+const Nav = () => {
+    return (
+        <div className="nav">
+            <NavLink to="/">Donors</NavLink>
+            
+        </div>
+    )
+}
+
+
+export default Nav; 
