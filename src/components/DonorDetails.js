23878923d8ef3c1f66035d66989626cad74ea015
umekow <umekolashun@gmail.com>
2019-10-21 19:48:03 -0500

    donor details

--- src/components/DonorDetails.js
+++ src/components/DonorDetails.js
@@ -5,7 +5,10 @@ const DonorDetails = props => {
     return (
         <div className="details">
             <h1>Name</h1>
-            
+            <p>Email</p>
+            <p>Telephone</p>
+            <p>Last Communication: 12/12/12</p>
+            <button href="#">Update Donor Details</button>
         </div>
     )
 }
