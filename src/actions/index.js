c6297bf6bdc6c4329be068f22ac635c2083dd02b
umekow <umekolashun@gmail.com>
2019-10-22 17:20:27 -0500

    actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,12 @@
 
 
-
+//login
+export const LOGIN = 'LOGIN'; 
+//signup
+export const SIGNUP = 'SIGNUP'; 
+//add donor
+export const ADD_DONOR = 'ADD_DONOR'; 
+//update communication
+export const UPDATE_DONOR = 'UPDATE_DONOR'; 
+//create campaign
+export const CREATE_CAMPAIGN = 'CREATE_CAMPAIGN'; 
