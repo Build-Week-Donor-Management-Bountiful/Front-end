7abc4727854dba3e98e6c16c601a10f6f7e4c3da
umekow <umekolashun@gmail.com>
2019-10-24 12:39:02 -0500

    trying again

--- src/components/CampaignDetails.js
+++ src/components/CampaignDetails.js
@@ -1,12 +1,22 @@
 import React from 'react'; 
 import DonorsList from './DonorsList'; 
+import { connect } from 'react-redux'; 
+
+
+const mapStateToProps = state => {
+    return {
+        campaigns: state.campaigns, 
+        donors: state.donors
+    }
+}
 const CampaignDetails = props => {
+    const campaign = props.campaigns.find(donor => donor.id === Number(props.match.params.id))
     return (
         <div className="details">
-           <h2>{props.name}</h2>
-            <img src="" alt={`${props.name}'s campaign photo`} />
-            <p>Goal: {props.goal}</p>
-            <p>Money Raised: {props.raised}</p>
+           <h2>{campaign.name}</h2>
+            <img src="" alt={`${campaign.name}'s campaign photo`} />
+            <p>Goal: {campaign.goal}</p>
+            <p>Money Raised: {campaign.raised}</p>
             {props.donors.map( donor => (
                 <div className="donor" key={donor.id}>
                     <h4>{donor.name}</h4>
